6ba1137f709040a2e2bb6e1ec4500d9a7518ad80
Muhamad Danang Priambodo <68808411+mcdanang@users.noreply.github.com>
2023-06-03 18:48:00 +0700

    add tailwind setup

---
+++ projects/client/tailwind.config.js
@@ -0,0 +1,32 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ["./src/**/*.{html,js}"],
+	theme: {
+		colors: {
+			green: {
+				100: "#E8F2EC",
+				200: "#53B97C",
+				300: "#4CAD73",
+				400: "#2E9D5B",
+				500: "#0EB177",
+			},
+			gray: {
+				100: "#F2F2F2",
+				200: "#BDBDBD",
+				300: "#828282",
+				400: "#4F4F4F",
+				500: "#333333",
+			},
+			red: "#EB5757",
+			blue: "#2D9CDB",
+			black: "#000000",
+			white: "#ffffff",
+			yellow: "#FFE082",
+		},
+		fontFamily: {
+			inter: ["Inter, sans-serif"],
+		},
+		extend: {},
+	},
+	plugins: [],
+};
