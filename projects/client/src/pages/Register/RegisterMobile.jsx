6a140fb82d75d0cd2f3fea5c9d9a4394bf10368b
Gameciel <christofer.calvin.pranata@gmail.com>
2023-06-05 00:35:52 +0700

    added RegisterMobile for RegisterPage

---
+++ projects/client/src/pages/Register/RegisterMobile.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import MobileBackButton from "../../components/Mobile/MobileBackButton.jsx";
+const RegisterMobile = () => {
+	return (
+		<div className="mx-auto w-[480px] h-screen bg-green-200">
+			<MobileBackButton pageName="Register" url="/" />
+		</div>
+	);
+};
+
+export default RegisterMobile;
---
+++ projects/client/src/pages/RegisterPage.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import RegisterMobile from "./Register/RegisterMobile.jsx";
+const RegisterPage = () => {
+	return <RegisterMobile />;
+};
+
+export default RegisterPage;
