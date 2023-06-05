3c7c2292640b6fbd73725a37f934a0e6ce85e0b1
Gameciel <christofer.calvin.pranata@gmail.com>
2023-06-05 19:05:33 +0700

    fix class to className

--- projects/client/src/components/BackButton.jsx
+++ projects/client/src/components/BackButton.jsx
@@ -2,7 +2,7 @@ const BackButton = ({ pageName, url }) => {
 	return (
 		<div className="flex flex-row items-center pl-5 py-5 w-full z-10">
 			<span className="material-icons text-white cursor-pointer">
-				<span class="material-symbols-outlined">chevron_left</span>
+				<span className="material-symbols-outlined">chevron_left</span>
 			</span>
 			<span className="text-white ml-6 text-xl">{pageName}</span>
 		</div>
