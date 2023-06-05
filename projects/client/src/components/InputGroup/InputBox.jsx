6d53ad369a66088395951ea5ee0a2225ee1c1b1b
Gameciel <christofer.calvin.pranata@gmail.com>
2023-06-05 19:06:13 +0700

    removed InputBoxIcon, fix class to className

--- projects/client/src/components/InputGroup/InputBox.jsx
+++ projects/client/src/components/InputGroup/InputBox.jsx
@@ -1,17 +1,21 @@
 import React from "react";
 
-const inputHandler = (setInput, event) => setInput(previousInput => ({...previousInput, [event.target.name]: event.target.value}));
+const inputHandler = (setInput, event) =>
+	setInput(previousInput => ({
+		...previousInput,
+		[event.target.name]: event.target.value,
+	}));
 
-const InputBox = ({ name, type, required, inputKey ,setInput }) => {
+const InputBox = ({ name, type, required, inputKey, setInput }) => {
 	return (
-		<div class="relative">
+		<div className="relative">
 			<input
 				type={type}
 				name={inputKey}
 				id={name}
 				required={required}
 				placeholder={name}
-				onChange={(event) => inputHandler(setInput, event)}
+				onChange={event => inputHandler(setInput, event)}
 				className="bg-gray-100 text-gray-500 placeholder-gray-200 border-2 border-white active:bg-white focus:border-green-500 focus:outline-none focus:border-2 focus:bg-white focus:text-black rounded-lg text-sm w-full p-2.5"
 			/>
 		</div>
