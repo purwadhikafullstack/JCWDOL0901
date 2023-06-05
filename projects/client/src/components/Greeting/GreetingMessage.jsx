f31f3b4a54b817bbcea03a6ef2702d8d5916fd44
Gameciel <christofer.calvin.pranata@gmail.com>
2023-06-05 20:01:10 +0700

    adjusted margin for layout

--- projects/client/src/components/Greeting/GreetingMessage.jsx
+++ projects/client/src/components/Greeting/GreetingMessage.jsx
@@ -5,7 +5,7 @@ const GreetingMessage = ({ navigate, email }) => {
 	return (
 		<div className="my-auto mx-auto flex flex-col items-center w-full pb-20">
 			<div className="text-xl font-semibold mt-6">Registration Success!</div>
-			<img className="w-[200px] my-4" src="/assets/images/email_check.png" />
+			<img className="w-[200px] my-12" src="/assets/images/email_check.png" />
 			<div className="bg-green-100 text-green-500 px-4 py-4 rounded-lg min-w-[65%]">
 				<p>Thank you for registering!</p>
 				<p>Check your email to verify account.</p>
