e081e09cf8f5ae198be486650a5adfea8a26aa48
Gameciel <christofer.calvin.pranata@gmail.com>
2023-06-06 19:22:27 +0700

    created SuccesBox for modularity

--- projects/client/src/components/Greeting/GreetingMessage.jsx
+++ projects/client/src/components/Greeting/GreetingMessage.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import Button from "../Button";
+import SuccessBox from "../SuccessBox";
 
 const GreetingMessage = ({ navigate, email }) => {
 	return (
 		<div className="my-auto mx-auto flex flex-col items-center w-full pb-20">
 			<div className="text-xl font-semibold mt-6">Registration Success!</div>
 			<img className="w-[200px] my-12" src="/assets/images/email_check.png" />
-			<div className="bg-green-100 text-green-500 px-4 py-4 rounded-lg min-w-[65%]">
+			<SuccessBox>
 				<p>Thank you for registering!</p>
 				<p>Check your email to verify account.</p>
 				<p className="font-bold text-sm mt-2">{email}</p>
@@ -14,7 +15,7 @@ const GreetingMessage = ({ navigate, email }) => {
 					name={"Login"}
 					onClickHandler={() => navigate("/login", { state: { email } })}
 				/>
-			</div>
+			</SuccessBox>
 		</div>
 	);
 };
