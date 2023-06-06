d336ce36a16cfb8ea9d2c12de74c5fa1e1ef734f
Muhamad Danang Priambodo <68808411+mcdanang@users.noreply.github.com>
2023-06-06 10:11:39 +0700

    add auto generate coordinate

--- projects/server/src/queries/Branches.js
+++ projects/server/src/queries/Branches.js
@@ -3,13 +3,21 @@ const { getCoordinates } = require("../utils/opencage.js");
 const { readCityQuery } = require("../queries/Data.js");
 
 const createBranchQuery = async (body, admin_id, transaction) => {
-	const { city_id, name, latitude, longitude } = body;
+	const { city_id, name } = body;
 	const city = await readCityQuery(city_id);
-	const address = `${city.type} ${city.name}, ${city.Province.name}`;
-	console.log(getCoordinates(address, process.env.OPENCAGE_API_KEY));
-
+	const { latitude, longitude } = await getCoordinates(
+		city.type,
+		city.name,
+		city.Province.name
+	);
 	return await Branches.create(
-		{ admin_id, city_id, name, latitude, longitude },
+		{
+			admin_id,
+			city_id,
+			name,
+			latitude,
+			longitude,
+		},
 		{ transaction }
 	);
 };
--- projects/server/src/utils/opencage.js
+++ projects/server/src/utils/opencage.js
@@ -1,35 +1,31 @@
 const opencage = require("opencage-api-client");
 
-const getCoordinates = async (address, token) => {
+const getCoordinates = async (type, city, province) => {
 	return new Promise(async (resolve, reject) => {
 		try {
-			opencage
-				.geocode({ q: address })
+			await opencage
+				.geocode({ q: `${type} ${city}, ${province}` })
 				.then(data => {
-					// console.log(JSON.stringify(data));
 					if (data.status.code === 200 && data.results.length > 0) {
-						console.log(
-							data.results.filter(
-								city =>
-									city.components._type == "county" ||
-									city.components._type == "city"
-							)
-						);
-					}
-				})
-				.catch(error => {
-					// console.log(JSON.stringify(error));
-					console.log("Error", error.message);
-					// other possible response codes:
-					// https://opencagedata.com/api#codes
-					if (error.status.code === 402) {
-						console.log("hit free trial daily limit");
-						console.log("become a customer: https://opencagedata.com/pricing");
+						const place = data.results[0];
+						resolve({
+							status: "OK",
+							type: place.components._type,
+							name: place.formatted,
+							latitude: place.geometry.lat,
+							longitude: place.geometry.lng,
+						});
+					} else {
+						console.log("Status", data.status.message);
+						console.log("total_results", data.total_results);
+						resolve({
+							status: "NOT FOUND",
+						});
 					}
 				});
-			resolve();
 		} catch (error) {
-			reject({ name: "nodemailerError", detail: { error } });
+			console.log(error);
+			reject({ name: "opencageError", detail: { error } });
 		}
 	});
 };
