49879a7e95e3ee1700959b67012f88d0fc09640c
Muhamad Danang Priambodo <68808411+mcdanang@users.noreply.github.com>
2023-06-06 22:48:05 +0700

    add desktop view

--- projects/client/src/pages/CreateBranchAdmin/CreateBranchAdminDesktop.jsx
+++ projects/client/src/pages/CreateBranchAdmin/CreateBranchAdminDesktop.jsx
@@ -1,31 +1,31 @@
 import React from "react";
 import BackButton from "../../components/BackButton";
-import RegisterForm from "../../components/Register/RegisterForm";
+import CreateBranchAdminForm from "../../components/CreateBranchAdmin/CreateBranchAdminForm";
 import CompanyLogo from "../../components/CompanyLogo";
-import RegisterIllustration from "../../components/Register/RegisterIllustration";
+import CreateBranchAdminIllustration from "../../components/CreateBranchAdmin/CreateBranchAdminIllustration.jsx";
 
 const DesktopIllustration = () => {
 	return (
 		<div className="my-auto mx-12">
-			<div className="mb-4 text-green-200 text-3xl font-light">
-				Create an account.
+			<div className="mb-4 text-green-100 text-3xl font-light">
+				Create Branch Admin
 			</div>
-			<RegisterIllustration className="w-[400px] pb-12" />
+			<CreateBranchAdminIllustration className="w-[200px] pb-12" />
 		</div>
 	);
 };
 
 const RegisterDesktop = () => {
 	return (
-		<div className="flex flex-col mx-auto my-auto flex-1 min-w-[480px] min-h-screen overflow-hidden bg-green-300">
+		<div className="flex flex-col mx-auto my-auto flex-1 min-w-[480px] h-screen overflow-hidden bg-green-100">
 			<div className="flex flex-row shrink-0 justify-around bg-white mx-auto my-auto rounded-lg min-w-fit shadow-lg">
-				<div className="flex flex-col shrink-0 pt-8 content-start min-w-[fit] content-center">
-					<BackButton url="/" color="text-green-400" />
+				<div className="flex flex-col shrink-0 pt-8 content-start min-w-[fit] content-center bg-green-400">
+					<BackButton url="/" color="text-green-100" />
 					<DesktopIllustration />
 				</div>
 				<div className="flex flex-col items-center shrink-0">
 					<CompanyLogo color="true" className="mt-8 w-[75px] cursor-pointer" />
-					<RegisterForm />
+					<CreateBranchAdminForm />
 				</div>
 			</div>
 		</div>
