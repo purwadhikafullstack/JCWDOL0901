afb6f949ba276816db28cc7d405c84dc21bbc1c4
Gameciel <christofer.calvin.pranata@gmail.com>
2023-06-06 15:20:31 +0700

    used doublequote instead of single quote

--- projects/client/src/components/BackButton.jsx
+++ projects/client/src/components/BackButton.jsx
@@ -1,9 +1,10 @@
-const BackButton = ({  url, color }) => {
-	
+const BackButton = ({ url, color }) => {
 	return (
 		<div className="flex flex-row items-center pl-5 py-5 w-full z-10">
-			<span className='material-icons cursor-pointer'>
-				<span className={"material-symbols-outlined " + color}>chevron_left</span>
+			<span className="material-icons cursor-pointer">
+				<span className={"material-symbols-outlined " + color}>
+					chevron_left
+				</span>
 			</span>
 		</div>
 	);
