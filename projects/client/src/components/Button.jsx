714cd7ab9b569904c831d96048cfc774491f1d0d
Gameciel <christofer.calvin.pranata@gmail.com>
2023-06-06 21:37:48 +0700

    using built in formik.isSubmitting instead of isBusy

--- projects/client/src/components/Button.jsx
+++ projects/client/src/components/Button.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 
-const Button = ({ name, type, onClickHandler = null }) => {
+const Button = ({ name, type, onClickHandler = null, disabled }) => {
 	return (
 		<button
 			type={type}
 			onClick={onClickHandler}
+			disabled={disabled}
 			className="text-white bg-green-300 py-3 px-6 mt-8 drop-shadow-md rounded-lg font-medium w-full"
 		>
 			{name}
