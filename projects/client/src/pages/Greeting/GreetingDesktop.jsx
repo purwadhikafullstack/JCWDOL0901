946341d8be5c90bdebe1159dc89fa04693c69341
Gameciel <christofer.calvin.pranata@gmail.com>
2023-06-06 13:23:04 +0700

    separated layout for desktop / mobile

---
+++ projects/client/src/pages/Greeting/GreetingDesktop.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import GreetingMessage from "../../components/Greeting/GreetingMessage";
+import CompanyLogo from "../../components/CompanyLogo";
+
+const GreetingDesktop = () => {
+	const location = useLocation();
+	const navigate = useNavigate();
+
+	React.useEffect(() => {
+		if (!location?.state?.fromRegister) navigate("/");
+	}, []);
+
+	return (
+		location?.state?.email && (
+			<div className="flex flex-col items-center mx-auto pb-24 min-w-[480px] h-screen bg-white flex flex-col">
+				<CompanyLogo color={true} className={"w-[80px] my-10 cursor-pointer"} />
+				<div>
+					<GreetingMessage navigate={navigate} email={location?.state?.email} />
+				</div>
+			</div>
+		)
+	);
+};
+
+export default GreetingDesktop;
--- projects/client/src/pages/Register/RegisterPage.jsx
+++ projects/client/src/pages/Register/RegisterPage.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import RegisterMobile from "./RegisterMobile.jsx";
+import RegisterDesktop from "./RegisterDesktop.jsx";
 
 const RegisterPage = () => {
 	return (
 		<div>
-			<RegisterMobile />
+			<RegisterDesktop />
+			{/* <RegisterMobile /> */}
 		</div>
 	);
 };
