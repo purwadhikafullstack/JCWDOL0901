55d7be936e6feabeeb4d3816d3135027f06ebd42
Gameciel <christofer.calvin.pranata@gmail.com>
2023-06-06 15:21:02 +0700

    Readjusted desktop layout

--- projects/client/src/pages/Register/RegisterDesktop.jsx
+++ projects/client/src/pages/Register/RegisterDesktop.jsx
@@ -6,7 +6,7 @@ import RegisterIllustration from "../../components/Register/RegisterIllustration
 
 const DesktopIllustration = () => {
 	return (
-		<div className="my-auto mx-12">
+		<div className="my-auto mx-12 mt-2 shrink">
 			<div className="mb-4 text-green-200 text-3xl font-light">
 				Create an account.
 			</div>
@@ -17,14 +17,14 @@ const DesktopIllustration = () => {
 
 const RegisterDesktop = () => {
 	return (
-		<div className="flex flex-col mx-auto my-auto flex-1 min-w-[480px] min-h-screen overflow-hidden bg-green-300">
-			<div className="flex flex-row shrink-0 justify-around bg-white mx-auto my-auto rounded-lg min-w-fit shadow-lg">
-				<div className="flex flex-col shrink-0 pt-8 content-start min-w-[fit] content-center">
+		<div className="flex flex-col mx-auto my-auto min-w-[480px] min-h-screen bg-green-300">
+			<div className="flex flex-row min-w-fit justify-around mx-auto my-auto rounded-lg shadow-lg bg-white">
+				<div className="flex flex-col shrink content-center pt-8 ml-2">
 					<BackButton url="/" color="text-green-400" />
+					<CompanyLogo color="true" className="mb-2 w-1/6 cursor-pointer mx-auto shrink"/>
 					<DesktopIllustration />
 				</div>
 				<div className="flex flex-col items-center shrink-0">
-					<CompanyLogo color="true" className="mt-8 w-[75px] cursor-pointer" />
 					<RegisterForm />
 				</div>
 			</div>
