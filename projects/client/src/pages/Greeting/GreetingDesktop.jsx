5fdb144203f7becacbb3f3b38e7b3f70ef7ed75e
Gameciel <christofer.calvin.pranata@gmail.com>
2023-06-06 20:53:53 +0700

    reduced redundancy

--- projects/client/src/pages/Greeting/GreetingDesktop.jsx
+++ projects/client/src/pages/Greeting/GreetingDesktop.jsx
@@ -1,25 +1,16 @@
 import React from "react";
-import { useLocation, useNavigate } from "react-router-dom";
+
 import GreetingMessage from "../../components/Greeting/GreetingMessage";
 import CompanyLogo from "../../components/CompanyLogo";
 
-const GreetingDesktop = () => {
-	const location = useLocation();
-	const navigate = useNavigate();
-
-	React.useEffect(() => {
-		if (!location?.state?.fromRegister) navigate("/");
-	}, []);
-
+const GreetingDesktop = ({ location, navigate }) => {
 	return (
-		location?.state?.email && (
-			<div className="flex flex-col items-center mx-auto pb-24 min-w-[480px] h-screen bg-white flex flex-col">
-				<CompanyLogo color={true} className={"w-[80px] my-10 cursor-pointer"} />
-				<div>
-					<GreetingMessage navigate={navigate} email={location?.state?.email} />
-				</div>
+		<div className="flex flex-col items-center mx-auto pb-24 min-w-[480px] h-screen bg-white flex flex-col">
+			<CompanyLogo color={true} className={"w-[80px] my-10 cursor-pointer"} />
+			<div>
+				<GreetingMessage navigate={navigate} email={location?.state?.email} />
 			</div>
-		)
+		</div>
 	);
 };
 
