3791fc5ec5e6a3e5be0c772d062c372245cdfb82
Gameciel <christofer.calvin.pranata@gmail.com>
2023-06-06 15:27:26 +0700

    color theme match

--- projects/client/src/pages/Register/RegisterDesktop.jsx
+++ projects/client/src/pages/Register/RegisterDesktop.jsx
@@ -7,7 +7,7 @@ import RegisterIllustration from "../../components/Register/RegisterIllustration
 const DesktopIllustration = () => {
 	return (
 		<div className="my-auto mx-12 mt-2 shrink">
-			<div className="mb-4 text-green-200 text-3xl font-light">
+			<div className="mb-4 text-green-100 text-3xl font-light">
 				Create an account.
 			</div>
 			<RegisterIllustration className="w-[400px] pb-12" />
@@ -17,11 +17,11 @@ const DesktopIllustration = () => {
 
 const RegisterDesktop = () => {
 	return (
-		<div className="flex flex-col mx-auto my-auto min-w-[480px] min-h-screen bg-green-300">
-			<div className="flex flex-row min-w-fit justify-around mx-auto my-auto rounded-lg shadow-lg bg-white">
-				<div className="flex flex-col shrink content-center pt-8 ml-2">
-					<BackButton url="/" color="text-green-400" />
-					<CompanyLogo color="true" className="mb-2 w-1/6 cursor-pointer mx-auto shrink"/>
+		<div className="flex flex-col mx-auto my-auto min-w-[480px] min-h-screen bg-black[.05]">
+			<div className="flex flex-row min-w-fit justify-around mx-auto my-auto rounded-lg shadow-xl bg-white">
+				<div className="flex flex-col shrink content-center pt-8 bg-green-300 rounded-l-lg">
+					<BackButton url="/" color="text-green-100" />
+					<CompanyLogo color={false} className="mb-2 w-1/6 cursor-pointer mx-auto shrink"/>
 					<DesktopIllustration />
 				</div>
 				<div className="flex flex-col items-center shrink-0">
