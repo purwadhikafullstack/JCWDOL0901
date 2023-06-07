b4ee4dd370da99e752a8f31d067a2d371995b657
Muhamad Danang Priambodo <68808411+mcdanang@users.noreply.github.com>
2023-06-07 23:14:14 +0700

    add dashboard card

---
+++ projects/client/src/components/Dashboard/DashboardBranchInfo.jsx
@@ -0,0 +1,14 @@
+const DashboardBranchInfo = () => {
+	return (
+		<>
+			<div className="mr-auto ml-6 text-sm text-left font-semibold">
+				Groseria Store Semarang
+			</div>
+			<div className="mr-auto ml-6 text-sm text-left">
+				Kota Semarang, Jawa Tengah
+			</div>
+		</>
+	);
+};
+
+export default DashboardBranchInfo;
--- projects/client/src/pages/Dashboard/DashboardMobile.jsx
+++ projects/client/src/pages/Dashboard/DashboardMobile.jsx
@@ -3,6 +3,9 @@ import BackButton from "../../components/BackButton.jsx";
 import DashboardIllustration from "../../components/Dashboard/DashboardIllustration.jsx";
 import CircularBackgroundDecoration from "../../components/CircularBackgroundDecoration.jsx";
 import DashBoardGrossIncomeChart from "../../components/Dashboard/DashBoardGrossIncomeChart.jsx";
+import DashboardBranchInfo from "../../components/Dashboard/DashboardBranchInfo.jsx";
+import DashboardCard from "../../components/Dashboard/DashboardCard.jsx";
+import { rupiah } from "../../helper/rupiah.js";
 
 const MobileIllustration = () => {
 	return (
@@ -11,12 +14,7 @@ const MobileIllustration = () => {
 				<div className="mt-2 mb-2 mr-auto ml-6 text-3xl text-left font-semibold">
 					Dashboard
 				</div>
-				<div className="mr-auto ml-6 text-sm text-left font-semibold">
-					Groseria Store Semarang
-				</div>
-				<div className="mr-auto ml-6 text-sm text-left">
-					Kota Semarang, Jawa Tengah
-				</div>
+				<DashboardBranchInfo />
 			</div>
 			<DashboardIllustration className="w-[250px] mr-auto ml-auto pr-4" />
 		</div>
@@ -29,9 +27,15 @@ const DashboardMobile = () => {
 			<CircularBackgroundDecoration />
 			<BackButton url="/admin/dashboard" color="text-white" />
 			<MobileIllustration />
-			<div className="w-32 h-20 shadow-md rounded-md bg-green-300/80 mb-4 place-self-center text-white py-2 px-3">
-				<p>Total Buyer</p>
-				<p className="text-2xl">120</p>
+			<h1 className="mb-2">All time</h1>
+			<div className="mx-12 flex flex-row gap-2">
+				<DashboardCard title="Buyer" value="120" valueSize="text-xl" />
+				<DashboardCard
+					title="Gross Income"
+					value={rupiah(5400000)}
+					valueSize="text-md"
+				/>
+				<DashboardCard title="Product Sold" value="2541" valueSize="text-xl" />
 			</div>
 			<DashBoardGrossIncomeChart />
 			<DashBoardGrossIncomeChart />
