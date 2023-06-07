f3d48f13f566a454d060d2293d3ea306dd1c89d0
Muhamad Danang Priambodo <68808411+mcdanang@users.noreply.github.com>
2023-06-07 09:13:03 +0700

    refactor opencage.js

--- projects/server/src/utils/opencage.js
+++ projects/server/src/utils/opencage.js
@@ -1,32 +1,28 @@
 const opencage = require("opencage-api-client");
 
+const determineSuccessMessage = data => {
+	if (data.status.code === 200 && data.results.length > 0) {
+		const place = data.results[0];
+		return {
+			status: "OK",
+			type: place.components._type,
+			name: place.formatted,
+			latitude: place.geometry.lat,
+			longitude: place.geometry.lng,
+		};
+	} else {
+		return {
+			status: "NOT FOUND",
+		};
+	}
+};
+
 const getCoordinates = async (type, city, province) => {
 	return new Promise(async (resolve, reject) => {
-		try {
-			await opencage
-				.geocode({ q: `${type} ${city}, ${province}` })
-				.then(data => {
-					if (data.status.code === 200 && data.results.length > 0) {
-						const place = data.results[0];
-						resolve({
-							status: "OK",
-							type: place.components._type,
-							name: place.formatted,
-							latitude: place.geometry.lat,
-							longitude: place.geometry.lng,
-						});
-					} else {
-						console.log("Status", data.status.message);
-						console.log("total_results", data.total_results);
-						resolve({
-							status: "NOT FOUND",
-						});
-					}
-				});
-		} catch (error) {
-			console.log(error);
-			reject({ name: "opencageError", detail: { error } });
-		}
+		await opencage
+			.geocode({ q: `${type} ${city}, ${province}` })
+			.then(data => resolve(determineSuccessMessage(data)))
+			.catch(error => reject({ name: "opencageError", detail: { error } }));
 	});
 };
 
