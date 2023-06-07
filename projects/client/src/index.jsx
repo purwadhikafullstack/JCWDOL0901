a78888cc18b6ff07d1ed77c4e8043ac721764e52
Gameciel <christofer.calvin.pranata@gmail.com>
2023-06-07 15:46:31 +0700

    imported css for carousel

--- projects/client/src/index.jsx
+++ projects/client/src/index.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import "@splidejs/react-splide/css";
 import App from "./App";
 import "material-icons/iconfont/material-icons.css";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
 	<React.StrictMode>
 		<BrowserRouter>
