fd4c3da76f23f62f9132c38c5995d213b41b42d1
Muhamad Danang Priambodo <68808411+mcdanang@users.noreply.github.com>
2023-06-08 11:32:09 +0700

    fix bug query from & to

--- projects/server/src/services/adminTransactionService.js
+++ projects/server/src/services/adminTransactionService.js
@@ -9,21 +9,26 @@ const getRawData = data =>
 		return {
 			label: moment(cur.updated_at).format("DD/MM/YYYY"),
 			amount: cur.amount,
+			productSold: cur.Transaction_details.reduce((sum, cur) => sum + cur.quantity, 0),
 		};
 	});
 
 const getLabels = (from, to) => {
-	const diffInDays = moment(to).diff(moment(from), "days");
-	return new Array(diffInDays + 1).fill(0).map((cur, index, arr) =>
-		moment()
-			.subtract(arr.length - 1 - index, "days")
-			.format("DD/MM/YYYY")
-	);
+	const diffInDays = moment(to).diff(moment(from), "days") + 1;
+	return new Array(diffInDays)
+		.fill(0)
+		.map((cur, index, arr) => moment(from).add(index, "days").format("DD/MM/YYYY"));
 };
 
 const getGrossIncomeData = (labels, rawData) => {
 	return labels.map(label =>
 		rawData.reduce((sum, raw) => (label === raw.label ? (sum += raw.amount) : (sum += 0)), 0)
+	);
+};
+
+const getProductSoldData = (labels, rawData) => {
+	return labels.map(label =>
+		rawData.reduce((sum, raw) => (label === raw.label ? (sum += raw.productSold) : (sum += 0)), 0)
 	);
 };
 
@@ -33,11 +38,13 @@ const getDashboardData = async (from, to, status, branch) => {
 	const labels = getLabels(from, to);
 	const grossIncomeData = getGrossIncomeData(labels, rawData);
 	const totalGrossIncome = getTotalGrossIncome(grossIncomeData);
+	const productSoldData = getProductSoldData(labels, rawData);
 	return {
 		rawData,
 		totalGrossIncome,
 		grossIncomeData,
 		labels,
+		productSoldData,
 	};
 };
 
@@ -46,7 +53,7 @@ module.exports = {
 		return new Promise(async (resolve, reject) => {
 			try {
 				from = from ? new Date(from) : new Date(0);
-				to = to ? new Date(to) : new Date();
+				to = to ? new Date(to) : new Date(new Date().toLocaleDateString());
 				const adminTransactionsData = await readAdminTransactionsQuery(from, to, status, branch);
 				return resolve(adminTransactionsData);
 			} catch (error) {
@@ -57,8 +64,11 @@ module.exports = {
 	startGetAdminDashboardData: async (from, to, status, branch) => {
 		return new Promise(async (resolve, reject) => {
 			try {
-				from = from ? new Date(from) : moment().subtract(7, "days");
-				to = to ? new Date(to) : new Date(new Date().toLocaleDateString());
+				from = from
+					? moment(from)
+					: moment(new Date().toISOString().split("T")[0]).subtract(6, "days");
+				to = to ? moment(to) : moment(new Date().toISOString().split("T")[0]);
+				console.log(from, to);
 				const DashboardData = getDashboardData(from, to, status, branch);
 				return resolve(DashboardData);
 			} catch (error) {
