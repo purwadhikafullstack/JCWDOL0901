377e9416e1280f2efc727353c04224b1136346ba
Gameciel <christofer.calvin.pranata@gmail.com>
2023-06-08 22:29:44 +0700

    added conditional for address

--- projects/client/src/components/AddressMenu.jsx
+++ projects/client/src/components/AddressMenu.jsx
@@ -1,14 +1,19 @@
 import React from "react";
 import AddressDropDown from "./AddressMenu/AddressDropDown";
+import { useSelector } from "react-redux";
 
+const LoginBeforeSelecting = () => {
+	return <div className="text-green-100 text-sm font-light mt-2 mr-1.5">You need to sign in</div>;
+};
 const AddressMenu = () => {
+	const app = useSelector(state => state.app);
 	return (
 		<div className="flex flex-col items-end text-right">
 			<div className="text-green-100 flex flex-row items-center">
 				<span className="material-symbols-rounded font-thin text-lg">location_on</span>
 				<div className="mx-1.5 font-light antialiased">Deliver to</div>
 			</div>
-			<AddressDropDown />
+			{app.hasLogged ? <AddressDropDown /> : <LoginBeforeSelecting />}
 		</div>
 	);
 };
--- projects/client/src/components/MobileHeader.jsx
+++ projects/client/src/components/MobileHeader.jsx
@@ -9,12 +9,7 @@ const MobileHeader = () => {
 	return (
 		<div className="flex flex-col">
 			<CircularBackgroundDecoration />
-			<CompanyLogo
-				color={false}
-				className="z-10 w-[40px] mx-auto my-3"
-				clickable={false}
-			/>
-
+			<CompanyLogo color={false} className="z-10 w-[40px] mx-auto my-3" clickable={false} />
 			<div className="flex flex-col justify-between items-between z-10 mx-6">
 				<SearchBar />
 				<div className="flex flex-row justify-between items-start mt-2">
