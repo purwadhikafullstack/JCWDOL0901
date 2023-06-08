d2f4309dda61c83b7ca370825056aca9596330c8
Gameciel <christofer.calvin.pranata@gmail.com>
2023-06-08 10:21:30 +0700

    mobile footer to include company headquarter address

--- projects/client/src/components/MobileFooter.jsx
+++ projects/client/src/components/MobileFooter.jsx
@@ -1,18 +1,11 @@
-import CompanyLogo from '../components/CompanyLogo'
+import CompanyContact from "./CompanyContact"
 
 const MobileFooter = () => {
   return (
     <div className='pb-28 mt-8 font-light text-sm w-full'>
         <div className='w-full py-2 bg-green-200 text-green-100 mb-5 text-medium font-semibold'>Got any question?</div>
         <div className='flex flex-row justify-around'>
-            <div className='flex flex-col items-center'>
-                <span className="material-icons-outlined mb-1">mail</span>
-                <div className='font-light'>super@groseria.com</div>
-            </div>
-            <div className='flex flex-col items-center'>
-                <span className="material-icons-outlined mb-1">support_agent</span>
-                <div className='font-light'>1-500-505</div>
-            </div>
+            <CompanyContact />
         </div>
     </div>
   )
