a3669e987cb5525eccfc3c8c9da26e1f509c6d1f
Gameciel <christofer.calvin.pranata@gmail.com>
2023-06-09 17:18:01 +0700

    minor fix for categories

--- projects/client/src/components/CategoryCarousel/SliderSlides.jsx
+++ projects/client/src/components/CategoryCarousel/SliderSlides.jsx
@@ -6,12 +6,17 @@ const SliderSlides = ({ categories }) => {
 	const app = useSelector(state => state.app);
 	return (
 		<Splide
-			options={{ perPage: app.mobileView ? 4.5 : 10, padding: 5, pagination: false, arrows: false }}
+			options={{
+				perPage: app.mobileView ? 4.5 : 10,
+				padding: 5,
+				pagination: false,
+				arrows: false,
+			}}
 		>
 			{categories.map((category, index) => {
 				return (
 					<SplideSlide key={index}>
-						<div className="flex flex-col items-center">
+						<div className="flex flex-col items-center mx-2">
 							<img
 								className="w-[75px] cursor-pointer"
 								src={category.image}
