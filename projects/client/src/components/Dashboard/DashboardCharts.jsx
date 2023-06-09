c1a7497b7f7b6355aed20cb5cd602ebf96724af9
Gameciel <christofer.calvin.pranata@gmail.com>
2023-06-09 18:22:01 +0700

    update package.json

--- projects/client/src/components/Dashboard/DashboardCharts.jsx
+++ projects/client/src/components/Dashboard/DashboardCharts.jsx
@@ -1,5 +1,5 @@
 import DashboardCards from "./DashboardCards";
-import DashboardGrossIncomeChart from "./DashboardGrossIncomeChart";
+import DashBoardGrossIncomeChart from "./DashBoardGrossIncomeChart";
 import DashboardProductSoldChart from "./DashboardProductSoldChart";
 import getDashboardData from "./helper/getDashboardDataHelper.js";
 import { useState, useEffect } from "react";
@@ -14,7 +14,7 @@ const DashboardCharts = ({ containerClass, chartClass }) => {
 			<DashboardCards />
 			<h1 className="mt-4 text-green-400 font-bold text-xl">Last 7 Days</h1>
 			<div className={containerClass}>
-				<DashboardGrossIncomeChart data={dashboardData} chartClass={chartClass} />
+				<DashBoardGrossIncomeChart data={dashboardData} chartClass={chartClass} />
 				<DashboardProductSoldChart data={dashboardData} chartClass={chartClass} />
 			</div>
 		</>
