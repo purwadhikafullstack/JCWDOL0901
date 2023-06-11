b864793d66ba9b3eaea8f283a2c0897c31fd5c17
Muhamad Danang Priambodo <68808411+mcdanang@users.noreply.github.com>
2023-06-11 13:45:43 +0700

    fix sidebar bug

--- projects/client/src/components/SideBar/SideBarContent.jsx
+++ projects/client/src/components/SideBar/SideBarContent.jsx
@@ -5,9 +5,7 @@ import SideBarMenu from "./SideBarMenu";
 export default function SideBarContent({ sidebarOpen, setSidebarOpen, navigation }) {
 	return (
 		<div
-			className={`flex-1 flex flex-col min-h-0 border-r border-green-400 bg-green-200 transition duration-500 ${
-				sidebarOpen ? "" : " -translate-x-72"
-			}`}
+			className={`flex-1 flex flex-col min-h-0 border-r border-green-400 bg-green-200 transition duration-500 `}
 		>
 			<SideBarCloseButton setSidebarOpen={setSidebarOpen} />
 			<SideBarMenu navigation={navigation} />
