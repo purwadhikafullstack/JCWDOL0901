bf6d2ce4eea7b434196e364681f00a688119c6b4
Gameciel <christofer.calvin.pranata@gmail.com>
2023-06-11 12:20:43 +0700

    adjusted import name for sanitizer

--- projects/server/src/routers/authRoute.js
+++ projects/server/src/routers/authRoute.js
@@ -1,20 +1,19 @@
 const {
-  registerUser,
-  getAdmins,
-  registerAdmin,
-  verifyUser,
-  loginAdmin,
+	registerUser,
+	getAdmins,
+	registerAdmin,
+	verifyUser,
+	loginAdmin,
 } = require("../controllers/authController");
-const {
-  getReferrerId,
-  getAdminsQueryParamsSanitizer,
-  isSuperAdmin,
-} = require("../middlewares/authMiddleware");
+
+const { getReferrerId, isSuperAdmin, isAdmin } = require("../middlewares/authMiddleware");
+
+const { getAdminsQuerySanitizer } = require("../middlewares/sanitizer.js");
 
 const router = require("express").Router();
 
-router.get("/admin/list", getAdminsQueryParamsSanitizer, getAdmins);
-router.post("/admin/register", isSuperAdmin, registerAdmin);
+router.get("/admin/list", isAdmin, getAdminsQuerySanitizer, getAdmins);
+router.post("/admin/register", isAdmin, isSuperAdmin, registerAdmin);
 router.post("/admin/login", loginAdmin);
 
 router.post("/user/register", getReferrerId, registerUser);
