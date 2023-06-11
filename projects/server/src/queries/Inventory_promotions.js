3a51a1fd649f23bf267dd731c510625769b157be
Gameciel <christofer.calvin.pranata@gmail.com>
2023-06-11 12:20:09 +0700

    added Inventory_promotions query file

--- projects/server/src/queries/Inventory_promotions.js
+++ projects/server/src/queries/Inventory_promotions.js
@@ -1,11 +1,36 @@
-const { Inventory_promotions } = require("../models/index.js");
+const { Inventory_promotions, Inventories, Promotions, Products } = require("../models/index.js");
 
 const createInventoryPromotionQuery = async data => {
-	await Inventory_promotions.create({ ...data });
+	return await Inventory_promotions.create({ ...data });
+};
+
+const readInventoryPromotionQuery = async (branch_id, filter, order) => {
+	return await Inventory_promotions.findAll({
+		where: filter?.Inventory_promotions,
+		include: [
+			{
+				model: Inventories,
+				where: { branch_id },
+				attributes: ["stock"],
+				include: { model: Products, attributes: { exclude: "id" } },
+			},
+			{ model: Promotions, attributes: ["name"] },
+		],
+		order: [...order?.Inventory_promotions],
+	});
 };
 
 const updateInventoryPromotionQuery = async (data, id) => {
-	await Inventory_promotions.update({ ...data }, { where: { id } });
+	return await Inventory_promotions.update({ ...data }, { where: { id } });
 };
 
-module.exports = { createInventoryPromotionQuery, updateInventoryPromotionQuery };
+const deleteInventoryPromotionQuery = async id => {
+	return await Inventory_promotions.destroy({ where: { id } });
+};
+
+module.exports = {
+	createInventoryPromotionQuery,
+	readInventoryPromotionQuery,
+	updateInventoryPromotionQuery,
+	deleteInventoryPromotionQuery,
+};
