a8338963b577e7bd1304d1ccb07b0e6bdf5a4a53
Gameciel <christofer.calvin.pranata@gmail.com>
2023-06-12 19:14:19 +0700

    Added PageTitle reusable component

---
+++ projects/client/src/components/PageTitle.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const PageTitle = ({ title }) => {
+	return <div className="mx-auto text-xl font-semibold text-green-100">{title}</div>;
+};
+
+export default PageTitle;
