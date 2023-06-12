4bc83be91c2b0f9728d98f770c4ddaa845ad9d85
Gameciel <christofer.calvin.pranata@gmail.com>
2023-06-12 19:15:04 +0700

    layout update

--- projects/client/src/components/SelectBranchModal/BranchModalDropDown.jsx
+++ projects/client/src/components/SelectBranchModal/BranchModalDropDown.jsx
@@ -5,17 +5,17 @@ import { getAllBranches } from "./handler/BranchModalHandler";
 const ToggleDropDown = ({ open, branch }) => {
 	return (
 		<Listbox.Button
-			className={`flex flex-row items-center justify-center border-2 border-green-200 px-4 py-2 rounded-lg ${
+			className={`bg-white flex flex-row items-center justify-center border-2 border-green-200 px-4 py-2 rounded-lg ${
 				open ? "mb-1" : "mb-36"
 			}`}
 		>
-			<div className="flex flex-col items-center ml-auto w-full px-2">
+			<div className="flex flex-col items-center ml-auto w-full">
 				<span className="text-xs whitespace-nowrap mx-auto mb-1">Switch To:</span>
-				<span className="font-semibold text-sm max-w-full overflow-x-hidden whitespace-nowrap mx-auto">
+				<span className="font-semibold text-sm max-w-[80%] overflow-x-hidden whitespace-nowrap ">
 					{branch.name || "..."}
 				</span>
 			</div>
-			<span className="material-symbols-rounded ml-auto">expand_more</span>
+			<span className="material-symbols-rounded">expand_more</span>
 		</Listbox.Button>
 	);
 };
@@ -38,7 +38,7 @@ const BranchLists = ({ branches, setBranch }) => {
 	return branches.map((branch, index) => {
 		return (
 			<Listbox.Option
-				className="border ml-2 py-2 px-4 mb-0.5 rounded-lg shadow whitespace-nowrap max-w-full"
+				className="border ml-2 py-2 px-4 mb-0.5 rounded-lg shadow whitespace-nowrap max-w-full bg-white"
 				key={index}
 				value={branch.id}
 				onClick={() => setBranch(branch)}
@@ -59,7 +59,7 @@ const DropDownOptions = ({ setBranch }) => {
 	}, []);
 
 	return (
-		<Listbox.Options className="w-full mx-auto max-h-24  overflow-y-scroll mb-11">
+		<Listbox.Options className="w-full mx-auto max-h-24  overflow-y-scroll mb-11 ">
 			<BranchLists branches={branches} setBranch={setBranch} />
 		</Listbox.Options>
 	);
