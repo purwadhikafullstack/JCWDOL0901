7cae0e4f824d23d459a03f0a3761970cd10056fc
Gameciel <christofer.calvin.pranata@gmail.com>
2023-06-13 21:06:53 +0700

    fix table

--- projects/client/src/components/ProductPromo/PromoTable.jsx
+++ projects/client/src/components/ProductPromo/PromoTable.jsx
@@ -21,7 +21,7 @@ const PromoTableHead = () => {
 
 const PromoTable = ({ filter, sort, order, page, setMaxPage }) => {
 	return (
-		<div className="flex overflow-x-auto rounded-lg border border-green-300 border-2">
+		<div className="flex overflow-x-auto rounded-lg border border-green-300 border-2 mx-4">
 			<table className="w-full">
 				<PromoTableHead />
 				<PromoTableBody
