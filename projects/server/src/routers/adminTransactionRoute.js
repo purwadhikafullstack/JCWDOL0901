db9d47fadad37e5c04b0f0c73230108554ae4345
Muhamad Danang Priambodo <68808411+mcdanang@users.noreply.github.com>
2023-06-13 12:27:47 +0700

    fix bug middleware branchdata

--- projects/server/src/routers/adminTransactionRoute.js
+++ projects/server/src/routers/adminTransactionRoute.js
@@ -3,12 +3,12 @@ const {
 	getAdminDashboardData,
 	getAdminAllTimeData,
 } = require("../controllers/adminTransactionController");
-const { getBranchId } = require("../middlewares/authMiddleware");
+const { isAdmin, getBranchId } = require("../middlewares/authMiddleware");
 
 const router = require("express").Router();
 
 router.get("/list", getAdminTransactions);
-router.get("/dashboard-data", getBranchId, getAdminDashboardData);
-router.get("/all-time-data", getBranchId, getAdminAllTimeData);
+router.get("/dashboard-data", isAdmin, getBranchId, getAdminDashboardData);
+router.get("/all-time-data", isAdmin, getBranchId, getAdminAllTimeData);
 
 module.exports = router;
