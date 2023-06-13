43268957dc5dcc2d51d890e94153fc3d973e7d32
Muhamad Danang Priambodo <68808411+mcdanang@users.noreply.github.com>
2023-06-13 21:13:11 +0700

    Update CategoryTable.jsx

--- projects/client/src/components/Category/CategoryTable.jsx
+++ projects/client/src/components/Category/CategoryTable.jsx
@@ -6,12 +6,9 @@ const CategoryTableHead = () => {
 	return (
 		<thead className="uppercase">
 			<tr className="text-center">
+				<th className={thClassName}>No</th>
 				<th className={thClassName}>Image</th>
 				<th className={thClassName}>Name</th>
-				<th className={thClassName}>Promo Type</th>
-				<th className={thClassName}>Value</th>
-				<th className={thClassName}>Start Date</th>
-				<th className={thClassName}>End Date</th>
 				<th className={thClassName}>Action</th>
 			</tr>
 		</thead>
