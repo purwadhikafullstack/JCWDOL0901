4f9a4b2e163c1c578078cbdd02c6f31b2ae16bf0
Gameciel <christofer.calvin.pranata@gmail.com>
2023-06-13 15:32:23 +0700

    unified GreetingPage

--- projects/client/src/pages/Greeting/GreetingPage.jsx
+++ projects/client/src/pages/Greeting/GreetingPage.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import Greeting from "./Greeting.jsx";
+import GreetingMessage from "../../components/Greeting/GreetingMessage";
+import CompanyLogo from "../../components/CompanyLogo";
 import { useLocation, useNavigate } from "react-router-dom";
 
 const GreetingPage = () => {
@@ -10,7 +11,16 @@ const GreetingPage = () => {
 		if (!location?.state?.fromRegister) navigate("/");
 	}, []);
 
-	return location?.state?.email && <Greeting location={location} navigate={navigate} />;
+	return (
+		location?.state?.email && (
+			<div className="flex flex-col items-center mx-auto pb-24 min-w-[480px] h-screen bg-white flex flex-col">
+				<CompanyLogo color={true} className={"w-[80px] my-10 cursor-pointer"} />
+				<div>
+					<GreetingMessage navigate={navigate} email={location?.state?.email} />
+				</div>
+			</div>
+		)
+	);
 };
 
 export default GreetingPage;
