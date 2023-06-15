e987dd7f89b7d92ff1c8231a10e1102a90ab98a7
Gameciel <christofer.calvin.pranata@gmail.com>
2023-06-15 15:15:04 +0700

    code cleanup

--- projects/client/src/components/ProductPromo/PromoTable/PromoTableBody.jsx
+++ projects/client/src/components/ProductPromo/PromoTable/PromoTableBody.jsx
@@ -7,7 +7,6 @@ const PromoTableBody = ({ name, filter, sort, order, page, setMaxPage }) => {
 
 	React.useEffect(() => {
 		const query = generateUrlQuery(name, page, filter, sort, order);
-		console.log(query);
 		getInventoryPromotions(query)
 			.then(result => {
 				setDatas(result.data.rows);
