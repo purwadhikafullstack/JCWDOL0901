c45d896d0a94e4965d6fdaaba46b2b051938c2ba
Muhamad Danang Priambodo <68808411+mcdanang@users.noreply.github.com>
2023-06-16 15:29:24 +0700

    update search bar layout

--- projects/client/src/components/Products/ProductsHeader.jsx
+++ projects/client/src/components/Products/ProductsHeader.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import CompanyLogo from "../CompanyLogo";
 import SearchBar from "../SearchBar";
 import DesktopNavBar from "../DesktopNavBar";
+import { AdjustmentsIcon } from "@heroicons/react/outline";
+import BackButton from "../BackButton";
 
 const ProductsHeader = ({}) => {
 	return (
@@ -10,7 +12,19 @@ const ProductsHeader = ({}) => {
 			<CompanyLogo color={true} className="z-10 w-[40px] mx-auto my-3 sm:hidden" clickable={false} />
 			<div className="flex flex-col justify-between items-between z-10 mx-6">
 				<DesktopNavBar />
-				<SearchBar />
+				<div className="flex flex-row items-center">
+					<div className="pr-5">
+						<BackButton url={-1} color="text-gray-400 sm:text-green-100 text-3xl" />
+					</div>
+					<SearchBar />
+					<button className="flex-1 flex items-center px-2 py-2 ml-6 text-base font-medium hover:bg-green-100/20 rounded-md">
+						<AdjustmentsIcon
+							className="text-gray-400 mr-4 flex-shrink-0 h-6 w-6 sm:text-green-100"
+							aria-hidden="true"
+						/>
+						<p className="hidden sm:block text-base font-medium text-gray-400 sm:text-green-100">Sort</p>
+					</button>
+				</div>
 			</div>
 		</div>
 	);
