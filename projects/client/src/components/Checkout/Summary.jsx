2dbfd868801c27f507c536224521af7ff4022731
Gameciel <christofer.calvin.pranata@gmail.com>
2023-06-19 15:20:08 +0700

    improved uis

--- projects/client/src/components/Checkout/Summary.jsx
+++ projects/client/src/components/Checkout/Summary.jsx
@@ -6,15 +6,15 @@ const Summary = () => {
 	return (
 		<div className="flex flex-col my-4 ml-auto mr-6">
 			<div className="grid grid-cols-8 gap-2 py-2 font-normal text-left">
-				<span className="col-start-5 col-span-2">Subtotal:</span>
+				<span className="col-start-4 col-span-2">Subtotal:</span>
 				<span className="col-start-7 col-span-2">Rp {summary.subtotal.toLocaleString("id")}</span>
 			</div>
 			<div className="grid grid-cols-8 gap-2 font-normal text-left">
-				<span className="col-start-5 col-span-2">Logistic fee:</span>
+				<span className="col-start-4 col-span-2">Logistic fee:</span>
 				<span className="col-start-7 col-span-2"> Rp {summary.logistic.toLocaleString("id")}</span>
 			</div>
 			<div className="grid grid-cols-8 gap-2 text-green-400 py-2 mt-2 font-bold text-left">
-				<span className="col-start-5 col-span-2">Total:</span>
+				<span className="col-start-4 col-span-2">Total:</span>
 				<span className="col-start-7 col-span-2">Rp {summary.total.toLocaleString("id")}</span>
 			</div>
 		</div>
