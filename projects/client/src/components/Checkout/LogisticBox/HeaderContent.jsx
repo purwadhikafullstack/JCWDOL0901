18d96707bada5a5983a5c799133c348c65339c1b
Gameciel <christofer.calvin.pranata@gmail.com>
2023-06-20 22:46:58 +0700

    disable change on empty cart

--- projects/client/src/components/Checkout/LogisticBox/HeaderContent.jsx
+++ projects/client/src/components/Checkout/LogisticBox/HeaderContent.jsx
@@ -1,14 +1,19 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 
 const HeaderContent = () => {
+	const summary = useSelector((state) => state.checkout.summary);
+
 	return (
 		<>
 			<span className="col-span-1 material-symbols-rounded">local_shipping</span>
 			<span className="col-span-4 text-left">Logistic:</span>
-			<Link className="col-start-7 underline cursor-pointer mr-2" to="logistic/change">
-				<span className="material-symbols-rounded text-sm">arrow_forward_ios</span>
-			</Link>
+			{summary.weight ? (
+				<Link className="col-start-7 underline cursor-pointer mr-2" to="logistic/change">
+					<span className="material-symbols-rounded text-sm">arrow_forward_ios</span>
+				</Link>
+			) : null}
 		</>
 	);
 };
--- projects/client/src/components/Checkout/VoucherBox/HeaderContent.jsx
+++ projects/client/src/components/Checkout/VoucherBox/HeaderContent.jsx
@@ -1,13 +1,17 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 const HeaderContent = () => {
+	const summary = useSelector((state) => state.checkout.summary);
 	return (
 		<>
 			<span className="col-span-1 material-symbols-rounded">local_activity</span>
 			<span className="col-span-4 text-left">Voucher</span>
-			<Link className="col-start-7 cursor-pointer mr-2" to="voucher/change">
-				<span className="material-symbols-rounded text-sm">arrow_forward_ios</span>
-			</Link>
+			{summary.total ? (
+				<Link className="col-start-7 cursor-pointer mr-2" to="voucher/change">
+					<span className="material-symbols-rounded text-sm">arrow_forward_ios</span>
+				</Link>
+			) : null}
 		</>
 	);
 };
