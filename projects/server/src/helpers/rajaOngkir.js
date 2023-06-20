ebc22890906d6c358b71a2fbd8889ebc8df4951f
Gameciel <christofer.calvin.pranata@gmail.com>
2023-06-20 12:21:12 +0700

    added rajaOngkir setup

---
+++ projects/server/src/helpers/rajaOngkir.js
@@ -0,0 +1,25 @@
+const request = require("request");
+
+const headers = {
+	key: `${process.env.RAJAONGKIR_API_KEY}`,
+	"content-type": "application/x-www-form-urlencoded",
+};
+
+module.exports = {
+	getRajaOngkirLogisticFee: async (branch_city_id, city_id, weight, courier) => {
+		return new Promise(async (resolve, reject) => {
+			var options = {
+				method: "POST",
+				url: `${process.env.RAJAONGKIR_API_URL}/cost`,
+				headers,
+				form: { origin: branch_city_id, destination: city_id, weight, courier },
+			};
+
+			await request(options, (error, response, body) => {
+				if (error) return reject(error);
+
+				return resolve(body);
+			});
+		});
+	},
+};
