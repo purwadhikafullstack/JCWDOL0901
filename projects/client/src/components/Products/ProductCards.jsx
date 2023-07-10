c3aa1bcc3d4c69d9c467b724d6d493c3e178e7dd
Muhamad Danang Priambodo <68808411+mcdanang@users.noreply.github.com>
2023-07-10 17:18:09 +0700

    fix search bar

--- projects/client/src/components/Products/ProductCards.jsx
+++ projects/client/src/components/Products/ProductCards.jsx
@@ -4,10 +4,10 @@ import { getProducts, generateUrlQuery } from "./handlers/productsHandler.js";
 import { useLocation } from "react-router-dom";
 import queryString from "query-string";
 
-const ProductCards = ({ page, itemPerPage, setMaxPage, filter, sort, order }) => {
+const ProductCards = ({ page, itemPerPage, setMaxPage, sort, order }) => {
 	const location = useLocation();
 	const parsed = queryString.parse(location.search);
-	const { category_id, branch_id } = parsed;
+	const { category_id, branch_id, filter } = parsed;
 
 	const [products, setProducts] = React.useState([]);
 
@@ -21,7 +21,7 @@ const ProductCards = ({ page, itemPerPage, setMaxPage, filter, sort, order }) =>
 			.catch((error) => setProducts([{ name: "Server Error!", image: "" }]));
 	}, [page, itemPerPage, branch_id, setMaxPage, category_id, filter, sort, order]);
 	return (
-		<div className="mx-4 my-7 grid grid-cols-2 sm:grid-cols-4 gap-4">
+		<div className="mx-4 my-7 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4">
 			{products.map((product, index) => {
 				return <ProductCard product={product} key={index} />;
 			})}
