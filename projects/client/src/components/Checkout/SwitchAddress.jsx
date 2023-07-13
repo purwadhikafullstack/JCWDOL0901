8b8c2245aa4c54d8cd3d4a18be723f3371640c37
Muhamad Danang Priambodo <68808411+mcdanang@users.noreply.github.com>
2023-07-13 19:05:50 +0700

    add continue button

--- projects/client/src/components/Checkout/SwitchAddress.jsx
+++ projects/client/src/components/Checkout/SwitchAddress.jsx
@@ -2,13 +2,17 @@ import React from "react";
 import BackButton from "../BackButton.jsx";
 import Addresses from "./SwitchAddress/Addresses.jsx";
 import { useNavigate } from "react-router-dom";
+import ContinueButton from "../ContinueButton.jsx";
 
 const SwitchAddress = () => {
 	const navigate = useNavigate();
 	return (
-		<div className="flex flex-col bg-white min-h-screen">
-			<BackButton color="text-green-400" url="/cart/checkout" />
-			<Addresses />
+		<div className="sm:p-20 min-h-screen bg-green-100 items-center justify-center">
+			<div className="flex flex-col min-h-screen sm:min-h-0 sm:p-5 bg-white max-w-7xl rounded-xl shadow-lg">
+				<BackButton color="text-green-400" url="/cart/checkout" />
+				<Addresses />
+				<ContinueButton />
+			</div>
 		</div>
 	);
 };
