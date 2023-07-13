c43a2574c99ab46510968fb2e17c2a09d0551b0a
Gameciel <christofer.calvin.pranata@gmail.com>
2023-07-13 10:45:38 +0700

    reverted bcrypt.js to original purpose

--- projects/server/src/utils/bcrypt.js
+++ projects/server/src/utils/bcrypt.js
@@ -4,20 +4,11 @@ const getHashPassword = async (password) => {
 	const salt = await bcrypt.genSalt(10);
 	const hashPassword = await bcrypt.hash(password, salt);
 
-	return console.log(hashPassword);
+	return hashPassword;
 };
 
 const verifyHashPassword = async (password, hashPassword) => {
 	return await bcrypt.compare(password, hashPassword);
 };
-
-getHashPassword("unverified");
-getHashPassword("gumigameciel");
-getHashPassword("demouser");
-getHashPassword("pelanggansmg");
-getHashPassword("marynara");
-getHashPassword("sansansan");
-getHashPassword("andrealenny");
-getHashPassword("budiharto");
 
 module.exports = { getHashPassword, verifyHashPassword };
