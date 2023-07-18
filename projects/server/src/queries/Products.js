988010f83c12538a95f21b750411daa6f2f36a71
Muhamad Danang Priambodo <68808411+mcdanang@users.noreply.github.com>
2023-07-18 10:06:17 +0700

    fix bug create new product

--- projects/server/src/queries/Products.js
+++ projects/server/src/queries/Products.js
@@ -80,7 +80,7 @@ const readProductsOnlyQuery = async (params) => {
 
 const createProductQuery = async (body, file) => {
 	const { name, price, description, weight, unit, category_id } = body;
-	const image = `/product/${file.filename}`;
+	const image = `uploads/products/${file.filename}`;
 	// const image = `/product/${file.filename}`;
 	return await Products.create({ name, image, price, description, weight, unit, category_id });
 };
@@ -88,7 +88,7 @@ const createProductQuery = async (body, file) => {
 const updateProductQuery = async (body, file, params) => {
 	const id = params.productId;
 	const { name, price, description, weight, unit, category_id, active } = body;
-	const image = file ? `/product/${file.filename}` : undefined;
+	const image = file ? `uploads/products/${file.filename}` : undefined;
 	return await Products.update(
 		{ name, price, description, weight, unit, image, category_id, active },
 		{ where: { id } },
