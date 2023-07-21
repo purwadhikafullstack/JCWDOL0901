2cf9cfed7ee5306ace3a4efb554c9218f8cd9501
Gameciel <christofer.calvin.pranata@gmail.com>
2023-07-21 11:34:06 +0700

    FIX crash on selecting logistic without having address

--- projects/client/src/components/Checkout/LogisticBox/HeaderContent.jsx
+++ projects/client/src/components/Checkout/LogisticBox/HeaderContent.jsx
@@ -3,13 +3,13 @@ import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 
 const HeaderContent = () => {
-	const summary = useSelector((state) => state.checkout.summary);
+	const checkout = useSelector((state) => state.checkout);
 
 	return (
 		<>
 			<span className="col-span-1 material-symbols-rounded">local_shipping</span>
 			<span className="col-span-4 text-left">Logistic:</span>
-			{summary.weight ? (
+			{checkout.summary.weight && checkout.address.id ? (
 				<Link className="col-start-7 underline cursor-pointer mr-2" to="logistic/change">
 					<span className="material-symbols-rounded text-sm">arrow_forward_ios</span>
 				</Link>
